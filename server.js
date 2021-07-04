78e794de52805baba8490fbf2dcb8bca20ce6ff8
Mayur Upadhayay <mayur123a@gmail.com>
2021-07-04 18:58:17 +0530

    Initial Preparation for Deployment on Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path');
 
 const db = require('./config/keys').mongoURI
 
@@ -38,5 +39,14 @@ const port = process.env.PORT || 5000
 app.use('/users', users)
 app.use('/profiles', profiles)
 app.use('/posts', posts)
+
+//Serve static assests if in production
+if(process.env.NODE_ENV === 'production'){
+    //Serve static files
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
