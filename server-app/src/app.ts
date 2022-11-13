7c0fb38248f5444cbc1039a769cfe91ac2746598
Mayur Upadhayay <mayur123a@gmail.com>
2022-11-13 19:46:56 +0530

    [feat]: Bootstrapped Express project with typescript

---
+++ server-app/src/app.ts
@@ -0,0 +1,26 @@
+// common package imports
+import express, { Application } from 'express';
+import { createServer, Server } from 'http';
+
+// Application class 
+class App {
+    private express: Application;
+    private httpServer: Server | undefined;
+
+    constructor() {
+        this.express = express();
+        this.initHttpServer();
+    }
+
+    initHttpServer(): void {
+        this.httpServer = createServer(this.express);
+    }
+
+    getHttpServer(): Server | undefined {
+        return this.httpServer;
+    }
+}
+
+const app = new App();
+
+export default app.getHttpServer();
